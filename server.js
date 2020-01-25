952a7a9e17fd32d4a6b0f1bf255dce7301734ea4
tanajeannai3 <tanajeannai3@gmail.com>
2020-01-25 12:02:26 -0600

    adding JAWSDB

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var express = require("express");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
 // Requiring our models for syncing
 var db = require("./models");
 
